3837b722a33ed8398f284f7a2dc449875b3e7d4b
ArcherHuang <mmosconii@gmail.com>
2019-08-30 13:35:25 +0800

    init demo-redis

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const redisController = require('../controllers/redisController.js')
+
+module.exports = app => {
+
+  app.get('/redis', redisController.getRedisKey)
+  app.post('/redis', redisController.setRedisKey)
+}
